18cfe0e6a80ba8108df8265fa2749f5884fc629d
Mustafa-Noor <mus.noor12@gmail.com>
2025-09-27 07:12:05 +0500

    Boilerplate code

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,6 +2,14 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],         # Allow all origins
+    allow_credentials=True,      # Allow cookies, authorization headers, etc.
+    allow_methods=["*"],         # Allow all HTTP methods (GET, POST, etc.)
+    allow_headers=["*"],         # Allow all headers
+)
+
 @app.get("/")
 def read_root():
     return {"Welcome": "to InnoScope Backend!"}
